434e0f022ec6560808ad84610b4874debbbef608
Marco Moosler <moosler@gmx.de>
2020-03-01 17:24:51 +0100

    grid added

--- main.js
+++ main.js
@@ -11,8 +11,27 @@ const _width = app.renderer.view.width;
 const _height = app.renderer.view.height;
 
 let grid = new Grid(_width, _height, 10, 10);
-app.stage.addChild(grid.drawGrid());
+
+// app.stage.addChild(grid.drawGrid());
+
+// grid.setTile(0, 0);
+// app.stage.addChild(grid.drawElement(0, 0));
+
+let style = {
+  color: "0xff0000",
+  width: 10,
+  alpha: 1
+};
+let pos = {};
+const container = new PIXI.Container();
+
+pos.y1 = 10;
+pos.x2 = 100;
+pos.x1 = 10;
+pos.y2 = 100;
+var line = drawLine(pos, style);
+container.addChild(line);
+
+app.stage.addChild(container);
 
 console.log(grid);
-
-// drawGrid();
