f3538d448654350b63f8a1d7ca0066f4ccb705f7
Marco Moosler <moosler@gmx.de>
2020-03-04 22:28:28 +0100

    gsap added

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+gsap.registerPlugin(MotionPathPlugin);
+
 const app = new PIXI.Application({
   width: 800,
   height: 500,
   antialias: true,
-  backgroundColor: 0xEEEEEE,
+  backgroundColor: 0xeeeeee,
   resolution: window.devicePixelRatio || 1
 });
 document.body.appendChild(app.view);
@@ -13,7 +15,6 @@ const _height = app.renderer.view.height;
 let game = new Game(_width, _height, app);
 game.start();
 
-
 /**Train */
 // let pos = grid.getMidpoint(5, 7);
 // let train = new Train({ x: pos.x, y: pos.y, r: grid.xWidth / 4 });
@@ -23,4 +24,3 @@ game.start();
 // app.ticker.add(delta => {
 //   train.container.y -= 1 * delta;
 // });
-
