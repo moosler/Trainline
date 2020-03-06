eb8ee8b9e3024cd9b3a4eb9c2533a8b390b5b5be
Marco Moosler <moosler@telemaxx.de>
2020-03-06 14:59:49 +0100

    motion path added

--- main.js
+++ main.js
@@ -9,11 +9,22 @@ const app = new PIXI.Application({
 });
 document.body.appendChild(app.view);
 
-const _width = app.renderer.view.width;
-const _height = app.renderer.view.height;
+// const _width = app.renderer.view.width;
+// const _height = app.renderer.view.height;
+const _width = 800;
+const _height = 500;
 
 let game = new Game(_width, _height, app);
 game.start();
+var tween = game.tween;
+
+document.querySelector("body").onload = function() {
+  document.querySelector("#play").onclick = () => tween.play();
+  document.querySelector("#pause").onclick = () => tween.pause();
+  document.querySelector("#resume").onclick = () => tween.resume();
+  document.querySelector("#reverse").onclick = () => tween.reverse();
+  document.querySelector("#restart").onclick = () => tween.restart();
+};
 
 /**Train */
 // let pos = grid.getMidpoint(5, 7);
